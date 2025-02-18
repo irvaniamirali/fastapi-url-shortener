465fb34953f2aaea35408f62589ff02234fe6194
AmirAli Irvani <irvaniamirali@proton.me>
2025-02-18 19:54:59 +0330

    feat: Add base of configs.

---
+++ app/configs.py
@@ -0,0 +1,14 @@
+
+app_config = {
+    "title": "ZipLink",
+    "description": "ZipLink is a simple FastAPI app to shorten URLs.",
+    "contact": {
+        "name": "AmirAli Irvani",
+        "url": "https://github.com/irvaniamirali/ziplink",
+        "email": "irvaniamirali@proton.me",
+    },
+    "license_info": {
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    }
+}
