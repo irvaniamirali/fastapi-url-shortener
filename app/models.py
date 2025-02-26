14e8f8c3199225fa05fa39613510fa70eb88680a
AmirAli Irvani <irvaniamirali@proton.me>
2025-02-27 02:09:45 +0330

    feat: Add Admin model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,15 @@
 from tortoise import Model, fields
 
 
+class Admin(Model):
+    id = fields.IntField(primary_key=True)
+    admin_key = fields.CharField(max_length=12, unique=True)
+
+
 class URL(Model):
     id = fields.IntField(primary_key=True)
-    url = fields.CharField(max_length=2000)  # RFC 3986
+    url = fields.CharField(max_length=255)
     key = fields.CharField(max_length=6, unique=True)
     clicks = fields.IntField(default=0)
     expire = fields.DatetimeField(null=True)
-    admin_url = fields.CharField(max_length=8, unique=True)
+    admin = fields.ForeignKeyField("models.Admin", related_name="urls")
