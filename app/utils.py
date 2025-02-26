d7535d0f1f5abd9ec13a2f6efbfdad7dee62f32c
AmirAli Irvani <irvaniamirali@proton.me>
2025-02-26 21:42:47 +0330

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,22 @@
 import random
 import string
+import asyncio
+import datetime
 
-def generate_random_string(length=6):
+def generate_random_string():
     letters = string.ascii_letters + string.digits
-    random_string = ''.join(random.choice(letters) for i in range(length))
-    return random_string
+    random_string = lambda length : ''.join(random.choice(letters) for i in range(length))
+    return random_string(6), random_string(8)
+
+
+async def wait_until(dt):
+    """
+    Sleep until the specified datetime.
+    """
+    now = datetime.datetime.now()
+    await asyncio.sleep((dt - now).total_seconds())
+
+
+async def run_task(run_time, coro):
+    await wait_until(run_time)
+    return await coro
