5adff766892e5fe76e651ec01e2db297ffe81cff
AmirAli Irvani <irvaniamirali@proton.me>
2025-02-26 21:38:31 +0330

    Disconnecting the Redirect router from the main router.

---
+++ app/routes/redirect.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Path, status
+from fastapi.responses import RedirectResponse
+from fastapi.exceptions import HTTPException
+from app.models import URL
+
+router = APIRouter(tags=["Redirect"])
+
+
+@router.get("/{key}", status_code=status.HTTP_200_OK)
+async def redirect(key: str = Path(..., max_length=6)) -> RedirectResponse:
+    """
+    Redirect the target URL associated with the provided key.
+
+    :param key: The key associated with the URL.
+    :return: A redirect response to the target URL if it exists; raises a 404 error if not found.
+    """
+    exist_url = await URL.filter(key=key).first()
+
+    if exist_url is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="This URL Key does not exist in the database; please create it first."
+        )
+
+    exist_url.clicks += 1
+    await exist_url.save()
+
+    return RedirectResponse(exist_url.url)
