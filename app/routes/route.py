5901c74063d0c434c60b4652b97df0d68f55a79e
AmirAli Irvani <irvaniamirali@proton.me>
2025-02-27 21:24:22 +0330

    Update route.py

--- app/routes/route.py
+++ app/routes/route.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
 
 from .admin import router as admin_router
-from .url import router as url_router
 from .redirect import router as redirect_router
 
 router = APIRouter()
 
 router.include_router(admin_router, prefix="/api")
-router.include_router(url_router, prefix="/api")
 router.include_router(redirect_router)
