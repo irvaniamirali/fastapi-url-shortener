324a3226fa3ad52f9e6ae519b3a6af76c3381cfa
AmirAli Irvani <irvaniamirali@proton.me>
2025-03-16 23:30:35 +0330

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,15 +1,7 @@
-from typing import Optional
-from passlib.context import CryptContext
-from jose import jwt
-
 import asyncio
 import random
 import string
 import datetime
-
-from app.configs import ALGORITHM, SECRET_KEY
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def generate_random_digit_number():
     first_digit = random.randint(1, 9)
@@ -31,19 +23,3 @@ async def wait_until(dt):
 async def run_task(run_time, coro):
     await wait_until(run_time)
     return await coro
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-def create_access_token(data: dict, expires_delta: Optional[datetime.timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
