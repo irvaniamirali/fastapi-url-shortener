ed8cbed79920772e0d9c91592af054c81be3d0e7
AmirAli Irvani <irvaniamirali@proton.me>
2025-03-16 12:34:30 +0330

    Delete database.py

--- app/database.py
+++
@@ -1,8 +0,0 @@
-from tortoise import Tortoise
-
-async def init():
-    await Tortoise.init(
-        db_url="sqlite://zip-link.sqlite3",
-        modules={"models": ["app.models"]}
-    )
-    await Tortoise.generate_schemas()
