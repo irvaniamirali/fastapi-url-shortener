50410185468d7d23ba721437c917f4f3f2d06d09
AmirAli Irvani <irvaniamirali@proton.me>
2025-03-16 12:28:16 +0330

    Update configs.py

--- app/configs.py
+++ app/configs.py
@@ -1,10 +1,20 @@
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 app_config = {
     "title": "ZipLink",
     "description": "ZipLink is a FastAPI-based URL shortener.",
     "contact": {
         "name": "AmirAli Irvani",
-        "url": "https://github.com/irvaniamirali/ziplink",
+        "url": "https://github.com/irvaniamirali/zip-link",
         "email": "irvaniamirali.dev@gmail.com",
     },
     "license_info": {
