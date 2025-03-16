5ed4dac6caec4339008d8e235d2757027118e255
AmirAli Irvani <irvaniamirali@proton.me>
2025-03-16 12:21:54 +0330

    Separate parts related to dependencies.

---
+++ app/dependencies.py
@@ -0,0 +1,30 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+
+from app.models.users import User
+from app.schema.token import TokenData
+from app.configs import SECRET_KEY, ALGORITHM
+
+
+async def get_user(user_id: int):
+    return await User.filter(user_id=user_id).first()
+
+async def get_current_user(token: str = Depends(OAuth2PasswordBearer(tokenUrl="token"))):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = int(payload.get("sub"))
+        if user_id is None:
+            raise credentials_exception
+        token_data = TokenData(user_id=user_id)
+    except JWTError:
+        raise credentials_exception
+    user = await get_user(user_id=token_data.user_id)
+    if user is None:
+        raise credentials_exception
+    return user
