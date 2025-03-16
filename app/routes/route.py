4ddd5042d60c0e6f8d8fc35c9d795a30e8cec4bd
AmirAli Irvani <irvaniamirali@proton.me>
2025-03-16 13:39:51 +0330

    Remove some files

--- app/routes/route.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from .admin import router as admin_router
-from .redirect import router as redirect_router
-
-router = APIRouter()
-
-router.include_router(admin_router, prefix="/api")
-router.include_router(redirect_router)
