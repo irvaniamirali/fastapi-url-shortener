e3a084af39f65223a4451e7312f45c9a48c75f4c
AmirAli Irvani <irvaniamirali@proton.me>
2025-03-18 23:56:10 +0330

    Added urls model path

--- test/base.py
+++ test/base.py
@@ -6,6 +6,11 @@ from app.configs import DATABASE_URL
 async def init():
    await Tortoise.init(
        db_url=DATABASE_URL,
-       modules={'models': ['app.models.users']}
+       modules={
+           'models': [
+               'app.models.users',
+               'app.models.urls'
+           ]
+       }
    )
    await Tortoise.generate_schemas()
