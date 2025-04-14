dae6003bf6059cfd980ce796202a2b5f80c6dedf
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-04-14 19:33:21 +0330

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import uvicorn
 
 from app.main import app
-from configs import HOST, PORT
+from configs import settings
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host=HOST, port=PORT)
+    uvicorn.run(app, host=settings.uvicorn_host, port=settings.uvicorn_port)
