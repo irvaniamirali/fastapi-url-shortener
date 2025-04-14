24fe679a6decf5d3e3f2ce9ecd71493433e92967
AmirAli Irvani <irvaniamirali.dev@gmail.com>
2025-04-14 19:38:14 +0330

    feat: add cors middleware

---
+++ app/middleware/cors.py
@@ -0,0 +1,11 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def setup_middleware_cors(app):
+    app.add_middleware(
+        CORSMiddleware,  # type: ignore
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
